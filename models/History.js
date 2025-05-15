448787ff4a91a21b1cc8676bdd30151ef3d51574
Suraj Prasanna <vaishnupriya.v2023cse@sece.ac.in>
2025-05-16 05:15:28 +0530

    added

--- models/History.js
+++ models/History.js
@@ -2,22 +2,22 @@
 const mongoose = require('mongoose');
 
 const historySchema = new mongoose.Schema({
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true,
-  },
-  medicineId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Medicine',
-  },
-  date: Date,
-  time: String,
-  status: {
-    type: String,
-    enum: ['Taken', 'Skipped', 'Missed'],
-    default: 'Missed',
-  },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    medicineId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Medicine',
+    },
+    date: Date,
+    time: String,
+    status: {
+        type: String,
+        enum: ['Taken', 'Skipped', 'Missed', 'Scheduled'], // Added 'Scheduled' to the enum
+        default: 'Missed',
+    },
 });
 
 module.exports = mongoose.model('History', historySchema);
